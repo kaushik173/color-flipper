8e339706f97055bdfd2ad0c35da0f2030ddef531
kaushik173 <kaushikrathod1007@gmail.com>
2023-11-24 16:13:12 +0530

    changes  variable name

--- script/hex.js
+++ script/hex.js
@@ -1,17 +1,17 @@
-const hexcolors = [0,1,2,3,4,5,6,7,8,9,0,"A","B","C","D","E","F"];
+const hex = [0,1,2,3,4,5,6,7,8,9,0,"A","B","C","D","E","F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
-    let hexSign = "#";
+    let hexcolors = "#";
     for (let i = 0; i < 6; i++) {
-       hexSign += hexcolors[getRandomNumber()];
+        hexcolors += hex[getRandomNumber()];
         
     }
-    color.textContent = hexSign;
-     document.body.style.backgroundColor = hexSign
+    color.textContent = hexcolors;
+     document.body.style.backgroundColor = hexcolors
 });
 
 function getRandomNumber(){
-    return Math.floor(Math.random() * hexcolors.length);
+    return Math.floor(Math.random() * hex.length);
 }
